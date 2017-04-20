af4f43b93d1faa1a9a8297489aaeecd0094edb4d
g15946sm <g15946sm@s20640.ACTSUDA>
2017-04-20 16:32:49 +0900

    FirstHelloWorldです。

---
+++ src/FirstHelloWorld/FirstHelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FirstHelloWorld;
+
+/**
+ *
+ * @author g15946sm
+ */
+public class FirstHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+    
+}
